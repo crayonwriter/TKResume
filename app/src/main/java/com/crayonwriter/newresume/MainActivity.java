8be35bc90d78b3653bb20e6afb33264550cb1ae7
Theda Rogers <crayonwriter@gmail.com>
2019-08-10 01:21:13 -0400

    Update MainActivity.java with tab positions and fragment pager adapter details

--- app/src/main/java/com/crayonwriter/newresume/MainActivity.java
+++ app/src/main/java/com/crayonwriter/newresume/MainActivity.java
@@ -1,7 +1,11 @@
 package com.crayonwriter.newresume;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Attach the SectionsPagerAdapter to the ViewPager
+        SectionsPagerAdapter pagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        ViewPager pager = findViewById(R.id.pager);
+        pager.setAdapter(pagerAdapter);
+    }
+
+    private class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return 5;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new HomeFragment();
+
+                case 1:
+                    return new ContactFragment();
+
+                case 2:
+                    return new SkillsFragment();
+
+                case 3:
+                    return new ExperienceFragment();
+
+                case 4:
+                    return new PortfolioFragment();
+            }
+            return null;
+        }
     }
 }
