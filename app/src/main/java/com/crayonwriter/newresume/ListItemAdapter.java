f5882b6c44f144577c3fd2f1f4f708c6df7b4060
Theda Rogers <crayonwriter@gmail.com>
2019-08-25 18:43:42 -0400

    Update: add variables and adapter's constructor

--- app/src/main/java/com/crayonwriter/newresume/ListItemAdapter.java
+++ app/src/main/java/com/crayonwriter/newresume/ListItemAdapter.java
@@ -4,7 +4,16 @@ import android.support.v7.widget.RecyclerView;
 
 public class ListItemAdapter extends RecyclerView.Adapter<ListItemAdapter.ViewHolder> {
 
+    private String[] company_name;
+    private String[] description_text;
+
+    public ListItemAdapter(String[] company_name, String[] description_text) {
+        this.company_name = company_name;
+        this.description_text = description_text;
+    }
+    
+
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        
+
     }
 }
