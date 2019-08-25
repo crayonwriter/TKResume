f96324b0edc3822980238813e5499bdda90f674f
Theda Rogers <crayonwriter@gmail.com>
2019-08-25 18:29:57 -0400

    Create and add code to ListItemAdapter file

---
+++ app/src/main/java/com/crayonwriter/newresume/ListItemAdapter.java
@@ -0,0 +1,10 @@
+package com.crayonwriter.newresume;
+
+import android.support.v7.widget.RecyclerView;
+
+public class ListItemAdapter extends RecyclerView.Adapter<ListItemAdapter.ViewHolder> {
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        
+    }
+}
