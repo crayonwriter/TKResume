e75b1e396dd7d7bec9a90dbdf4d921838b89d897
Theda Rogers <crayonwriter@gmail.com>
2019-08-25 21:28:41 -0400

    Update: Added minor code

--- app/src/main/java/com/crayonwriter/newresume/ExperienceFragment.java
+++ app/src/main/java/com/crayonwriter/newresume/ExperienceFragment.java
@@ -3,6 +3,7 @@ package com.crayonwriter.newresume;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,17 +14,10 @@ import android.view.ViewGroup;
  */
 public class ExperienceFragment extends Fragment {
 
-
-    public ExperienceFragment() {
-        // Required empty public constructor
-    }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_experience, container, false);
+        RecyclerView experienceRecycler = (RecyclerView)inflater.inflate(R.layout.)
     }
 
 }
