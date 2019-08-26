c68c9d8479431a96181213698fb0d24485ac7c92
Theda Rogers <crayonwriter@gmail.com>
2019-08-25 21:51:39 -0400

    Create Experience class and insert dummy data and create getter methods

---
+++ app/src/main/java/com/crayonwriter/newresume/Experience.java
@@ -0,0 +1,24 @@
+package com.crayonwriter.newresume;
+
+public class Experience {
+    private String name;
+    private String description;
+
+    public static final Experience[] experiences = {
+            new Experience("Fannie Mae", "Proofread documents quickly and accurately using company style guide and extensive AP style knowledge, while working remotely."),
+            new Experience("Synchrony Bank", "Proofread terms and conditions for mutiple Synchrony Bank clients")
+    };
+
+    private Experience(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
