c2beedf2f17d34379e128e7ff0d379be0f723e95
Theda Rogers <crayonwriter@gmail.com>
2019-08-25 21:42:02 -0400

    Update: Complete code, including the experience recycler, in the experience fragment

--- app/src/main/java/com/crayonwriter/newresume/ExperienceFragment.java
+++ app/src/main/java/com/crayonwriter/newresume/ExperienceFragment.java
@@ -17,7 +17,21 @@ public class ExperienceFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        RecyclerView experienceRecycler = (RecyclerView)inflater.inflate(R.layout.)
+        RecyclerView experienceRecycler = (RecyclerView)inflater.inflate(R.layout.fragment_experience, container, false);
+
+        String[] experienceNames = new String[Experience.experiences.length];
+        for (int i=0; i < experienceNames.length; i++) {
+            experienceNames[i] = Experience.experiences[i].getName();
+        }
+
+        String[] experienceDescription = new String[Experience.experiences.length];
+        for (int i = 0; i < experienceDescription.length; i++) {
+            experienceDescription[i] = Experience.experiences[i].getDescription();
+        }
+
+        ListItemAdapter adapter = new ListItemAdapter(experienceNames, experienceDescription);
+        experienceRecycler.setAdapter(adapter);
+        return experienceRecycler;
     }
 
 }
