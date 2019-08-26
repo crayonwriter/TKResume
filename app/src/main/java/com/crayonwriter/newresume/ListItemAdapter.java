2906be79e4fc118b95ec1bb372566893238b81df
Theda Rogers <crayonwriter@gmail.com>
2019-08-25 21:25:22 -0400

    Update: Code ListItemAdapter with methods onBindViewHolder and onCreateViewHolder

--- app/src/main/java/com/crayonwriter/newresume/ListItemAdapter.java
+++ app/src/main/java/com/crayonwriter/newresume/ListItemAdapter.java
@@ -1,6 +1,15 @@
 package com.crayonwriter.newresume;
 
+import android.support.v7.view.menu.MenuView;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ListItemAdapter extends RecyclerView.Adapter<ListItemAdapter.ViewHolder> {
 
@@ -18,6 +27,26 @@ public class ListItemAdapter extends RecyclerView.Adapter<ListItemAdapter.ViewHo
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView company, description;
 
+        public ViewHolder(View view) {
+            super(view);
+            company = (TextView) view.findViewById(R.id.company_name);
+            description = view.findViewById(R.id.description_text);
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.listitem_layout, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        View itemView = holder.itemView;
+        TextView nameView = itemView.findViewById(R.id.company_name);
+        nameView.setText(company_name[position]);
+        TextView descriptionView = itemView.findViewById(R.id.description_text);
+        descriptionView.setText(description_text[position]);
     }
 }
