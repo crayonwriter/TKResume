7dd8b3fb41a7ff03b2d9f7918af5697b9be92f67
Theda Rogers <crayonwriter@gmail.com>
2019-09-06 15:26:08 -0400

    Update layout and add 2 more textviews

--- app/src/main/java/com/crayonwriter/newresume/Experience.java
+++ app/src/main/java/com/crayonwriter/newresume/Experience.java
@@ -5,8 +5,9 @@ public class Experience {
     private String description;
 
     public static final Experience[] experiences = {
-            new Experience("Fannie Mae", "Proofread documents quickly and accurately using company style guide and extensive AP style knowledge, while working remotely."),
-            new Experience("Synchrony Bank", "Proofread terms and conditions for mutiple Synchrony Bank clients")
+            new Experience("Proofreader", "Fannie Mae: Proofread documents quickly and accurately using company style guide and extensive AP style knowledge, while working remotely."),
+            new Experience("Proofreader", "Synchrony Bank: Proofread terms and conditions for mutiple Synchrony Bank clients"),
+
     };
 
     private Experience(String name, String description) {
--- app/src/main/java/com/crayonwriter/newresume/ListItemAdapter.java
+++ app/src/main/java/com/crayonwriter/newresume/ListItemAdapter.java
@@ -10,10 +10,14 @@ public class ListItemAdapter extends RecyclerView.Adapter<ListItemAdapter.ViewHo
 
     private String[] company_name;
     private String[] description_text;
+    private String[] description2_text;
+    private String[] description3_text;
 
-    public ListItemAdapter(String[] company_name, String[] description_text) {
+    public ListItemAdapter(String[] company_name, String[] description_text, String[]description2_text, String[] description3_text) {
         this.company_name = company_name;
         this.description_text = description_text;
+        this.description2_text = description2_text;
+        this.description3_text = description3_text;
     }
 
     @Override
@@ -22,12 +26,14 @@ public class ListItemAdapter extends RecyclerView.Adapter<ListItemAdapter.ViewHo
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        public View company, description;
+        public View company, description, description2, description3;
 
         public ViewHolder(View view) {
             super(view);
             company = view.findViewById(R.id.company_name);
             description = view.findViewById(R.id.description_text);
+            description2 = view.findViewById(R.id.description2_text);
+            description3 = view.findViewById(R.id.description3_text);
         }
     }
 
@@ -44,5 +50,7 @@ public class ListItemAdapter extends RecyclerView.Adapter<ListItemAdapter.ViewHo
         nameView.setText(company_name[position]);
         TextView descriptionView = itemView.findViewById(R.id.description_text);
         descriptionView.setText(description_text[position]);
+        TextView description2View = itemView.findViewById(R.id.description2_text);
+        description2View.setText(description2_text[position]);
     }
 }
