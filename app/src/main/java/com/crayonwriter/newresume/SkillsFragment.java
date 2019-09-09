996d5d41c680fd0334f1f9fab7c4ada672a00252
Theda Rogers <crayonwriter@gmail.com>
2019-09-09 08:27:34 -0400

    Update: make skills java file include RecyclerView

--- app/src/main/java/com/crayonwriter/newresume/SkillsFragment.java
+++ app/src/main/java/com/crayonwriter/newresume/SkillsFragment.java
@@ -2,22 +2,33 @@ package com.crayonwriter.newresume;
 
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
-public class SkillsFragment extends ListFragment {
-
+public class SkillsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                inflater.getContext(),
-                android.R.layout.simple_list_item_1,
-                getResources().getStringArray(R.array.skills));
-        setListAdapter(adapter);
-        return super.onCreateView(inflater, container, savedInstanceState);
-    }
+        RecyclerView experienceRecycler = (RecyclerView)inflater.inflate(R.layout.fragment_skills, container, false);
 }
+
+
+
+//public class SkillsFragment extends ListFragment {
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+//                inflater.getContext(),
+//                android.R.layout.simple_list_item_1,
+//                getResources().getStringArray(R.array.skills));
+//        setListAdapter(adapter);
+//        return super.onCreateView(inflater, container, savedInstanceState);
+//    }
+//}
